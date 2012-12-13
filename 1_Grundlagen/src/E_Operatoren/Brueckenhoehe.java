79416071aab3a128076bdbad7f1a4071f11abb43
Nikolai Hammer <hammer.nikolai@googlemail.com>
2012-12-13 16:50:29 +0100

    Eingabe mit Scanner Klasse

--- 1_Grundlagen/src/E_Operatoren/Brueckenhoehe.java
+++ 1_Grundlagen/src/E_Operatoren/Brueckenhoehe.java
@@ -1,19 +1,26 @@
-import hgsTools.*;
+package E_Operatoren;
+
+import java.util.Scanner;
+
+
 public class Brueckenhoehe {
 
     public static void main(String[] args) {
+    	Scanner s = new Scanner(System.in);
+    	
         //Hauptprogramm
         double g = 9.81; //Erdbeschleunigung
         double strecke;
         
         //Eingabe
-        double zeit = Eingabe.Double("Zeit:");
+        System.out.print("Zeit:");
+        double zeit = s.nextDouble();
                 
         //Verarbeitung
         strecke = 0.5*g*zeit*zeit;
         
         //Ausgabe
-        System.out.println("Die Höhe der Brücke:"+strecke);
+        System.out.println("Die Hšhe der BrŸcke:"+strecke);
         
 
     }
