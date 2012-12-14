5dedd7f99600941219979a58c619e13c528ff118
Nikolai Hammer <hammer.nikolai@googlemail.com>
2012-12-14 10:13:10 +0100

    Eingabe von Rate die Zahl mit Scanner Klasse

---
+++ 1_Grundlagen/src/G_Schleifen/ErrateDieZahl.java
@@ -0,0 +1,44 @@
+package G_Schleifen;
+
+import java.util.Scanner;
+
+
+public class ErrateDieZahl {
+	
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		int anzahlVersuche = 0;
+		int zz,zahl,diff,diffVorher=100;
+		
+		zz = (int) (Math.random()*100+1);
+		do {
+			System.out.println("-----------------");
+			System.out.print("Raten Sie: ");
+			zahl = s.nextInt();
+			diff = Math.abs(zz-zahl);
+			//System.out.println(zz+" "+zahl+" "+diff);
+			
+			if(diff<diffVorher){
+				diffVorher = diff;
+				System.out.println("heiß");
+			}
+			else if (diff==diffVorher){
+				System.out.println("lauwarm");
+				
+			}
+			else{
+				System.out.println("kalt");
+			}
+			
+			anzahlVersuche++;
+					
+		} while (diff != 0);
+		
+		System.out.println("Sie haben gewonnen mit "+anzahlVersuche+" Versuchen");
+		
+		
+	}
+
+}
