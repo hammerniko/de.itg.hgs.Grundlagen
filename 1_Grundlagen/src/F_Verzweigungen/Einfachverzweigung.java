af2b3ef5151989dc8573dde7f60d506b2a68f299
Hammer Nikolai <niko.hammer@hgs-singen.de>
2012-12-18 14:10:32 +0100

    Verzeigung hinzugefügt

---
+++ 1_Grundlagen/src/F_Verzweigungen/Einfachverzweigung.java
@@ -0,0 +1,38 @@
+package F_Verzweigungen;
+
+public class Einfachverzweigung {
+
+    //Hauptprogramm
+    public static void main(String[] args) {
+       
+        int a=120;
+        int b= 100;
+        
+        
+        if (a>b) {
+            System.out.println("a ist grš§er b");
+            
+        }
+        else {
+            System.out.println("a ist kleiner b");
+        }
+        
+        
+        
+        if (a>=100) {
+            System.out.println("a ist grš§er 100");
+        }
+                
+        
+        
+        
+        System.out.println("Programmende");
+        
+        
+        
+        
+        
+
+    }
+
+}
