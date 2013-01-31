a3e5906aad869f1af6e52435a208631c129f7f8d
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-01-31 13:29:18 +0100

    file added

---
+++ 1_Grundlagen/src/D_Eingabe/ErsteEingabe.java
@@ -0,0 +1,28 @@
+package D_Eingabe;
+
+import java.util.Scanner;
+
+public class ErsteEingabe {
+
+    
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.println("Grš§e:");
+        int groesse = s.nextInt();
+        System.out.println("Sie sind "+groesse+" cm gro§");
+        
+        System.out.println("Gewicht:");
+        double gewicht = s.nextDouble();
+        System.out.println("Sie wiegen "+gewicht+"kg");
+        
+        System.out.println("Name:");
+        String name = s.next();
+        System.out.println("Ihr Name ist "+name);
+        
+        
+        
+
+    }
+
+}
---
+++ 1_Grundlagen/src/I_Methoden/Methoden1.java
@@ -0,0 +1,21 @@
+package I_Methoden;
+
+public class Methoden1 {
+
+    public static void zeichneLinie() {
+        System.out.println("-----------");
+    }
+
+    public static void main(String[] args) {
+
+        zeichneLinie();
+        ausgabeHallo();
+        zeichneLinie();
+
+    }
+
+    public static void ausgabeHallo() {
+        System.out.println("Hallo");
+    }
+
+}
