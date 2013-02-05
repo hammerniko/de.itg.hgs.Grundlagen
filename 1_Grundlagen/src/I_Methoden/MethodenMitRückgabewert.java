cc32a329d1ba40ebff3efdef68c3f1f5d5a0400c
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-02-05 12:44:08 +0100

    file added

---
+++ 1_Grundlagen/src/I_Methoden/MethodenMitRückgabewert.java
@@ -0,0 +1,21 @@
+package I_Methoden;
+
+public class MethodenMitRŸckgabewert {
+    
+    public static void main(String[] args){
+ 
+        System.out.println(addiere(4,22));
+        
+    }
+    
+    
+    
+    public static double addiere(double zahl1, double zahl2){
+        double returnValue;
+        returnValue=zahl1+zahl2;
+        
+        return returnValue;
+    }
+    
+
+}
