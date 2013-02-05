605e3ab524b104f7040d9b171a2942df11567fc2
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-02-05 12:31:12 +0100

    file added

--- 1_Grundlagen/src/I_Methoden/Methoden1.java
+++ 1_Grundlagen/src/I_Methoden/Methoden1.java
@@ -2,20 +2,32 @@ package I_Methoden;
 
 public class Methoden1 {
 
-    public static void zeichneLinie() {
-        System.out.println("-----------");
+    public static void main(String[] args) {
+        zeichneLinie();
+        zeichneLinie(30);
+        zeichneLinie(25,'x');
     }
 
-    public static void main(String[] args) {
-
-        zeichneLinie();
-        ausgabeHallo();
-        zeichneLinie();
-
+    public static void zeichneLinie(int anzahl, char zeichen) {
+        for (int i = 0; i < anzahl; i++) {
+            System.out.print(zeichen);
+        }
+        System.out.println();
     }
 
     public static void ausgabeHallo() {
         System.out.println("Hallo");
+    }
+
+    public static void zeichneLinie() {
+        System.out.println("-----------");
+    }
+
+    public static void zeichneLinie(int anzahl) {
+        for (int i = 0; i < anzahl; i++) {
+            System.out.print("-");
+        }
+        System.out.println();
     }
 
 }
