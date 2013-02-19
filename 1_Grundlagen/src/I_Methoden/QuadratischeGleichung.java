cbc519d41f72046417d12c2472947d439fa1d4e8
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-02-19 14:00:09 +0100

    method added

--- 1_Grundlagen/src/I_Methoden/QuadratischeGleichung.java
+++ 1_Grundlagen/src/I_Methoden/QuadratischeGleichung.java
@@ -10,50 +10,65 @@ import java.util.Scanner;
 //Diskriminante <0 -> keine Lšsung
 //diskriminante =0 -> 1 Lšsung
 public class QuadratischeGleichung {
+    // Eingabe
+    static double a, b, c, d;
 
     public static void main(String[] args) {
-        //Eingabe
-        double x1,x2,a,b,c,d,diskriminante;
+
+        // Eingabe
         String lšsung;
+        eingabe();
+
+        // Verarbeitung
+        lšsung = loeseQuadratischeGleichung(a, b, c, d);
+
+        // Ausgabe
+        System.out.println(lšsung);
+    }
+
+    private static String loeseQuadratischeGleichung(double a, double b,
+            double c, double d) {
+        double x1;
+        double x2;
+        double diskriminante;
+        String lšsung;
+        // 7x^2+3x-17=0
+        c = c - d;
+        d = 0;
+        // x^2 + 3/7x -17/7 = 0
+        b = b / a;
+        c = c / a;
+        a = 1;
+        // Diskriminante
+        diskriminante = (b * b / 4) - c;
+        if (diskriminante > 0) {
+            x1 = -b / 2 + Math.sqrt(diskriminante);
+            x2 = -b / 2 - Math.sqrt(diskriminante);
+            lšsung = "x1 = " + x1 + "\nx2 = " + x2;
+        } else if (diskriminante == 0) {
+            x1 = -b / 2;
+            lšsung = "x1 = " + x1;
+        } else {
+            lšsung = "Es gibt keine Lšsung";
+        }
+        return lšsung;
+    }
+
+    public static void eingabe() {
         Scanner s = new Scanner(System.in);
-        
-        do {
-            //7x^2+3x-5=12
-            System.out
-                    .println("Geben sie die Koeffizienten der Quadratischen Gleichung ein:");
-            System.out.print("a: ");
-            a = s.nextDouble();
-            System.out.print("b: ");
-            b = s.nextDouble();
-            ;
-            System.out.print("c: ");
-            c = s.nextDouble();
-            ;
-            System.out.print("d: ");
-            d = s.nextDouble();
-            //Verarbeitung
-            //7x^2+3x-17=0
-            c = c - d;
-            d = 0;
-            //x^2 + 3/7x -17/7 = 0
-            b = b / a;
-            c = c / a;
-            a = 1;
-            //Diskriminante     
-            diskriminante = (b * b / 4) - c;
-            if (diskriminante > 0) {
-                x1 = -b / 2 + Math.sqrt(diskriminante);
-                x2 = -b / 2 - Math.sqrt(diskriminante);
-                lšsung = "x1 = " + x1 + "\nx2 = " + x2;
-            } else if (diskriminante == 0) {
-                x1 = -b / 2;
-                lšsung = "x1 = " + x1;
-            } else {
-                lšsung = "Es gibt keine Lšsung";
-            }
-            //Ausgabe
-            System.out.println(lšsung);
-            System.out.println("Verlassen mit einer Taste");
-        } while (!s.next().equalsIgnoreCase("q"));
+        // 7x^2+3x-5=12
+        System.out
+                .println("Geben sie die Koeffizienten der Quadratischen Gleichung ein:");
+        System.out.print("a: ");
+        a = s.nextDouble();
+        System.out.print("b: ");
+        b = s.nextDouble();
+        ;
+        System.out.print("c: ");
+        c = s.nextDouble();
+        ;
+        System.out.print("d: ");
+        d = s.nextDouble();
+
     }
 }
---
+++ 1_Grundlagen/src/I_Methoden/Rechner.java
@@ -0,0 +1,16 @@
+package I_Methoden;
+
+public class Rechner {
+
+    public static void main(String[] args) {
+        int ergebnis = addiere(3, 4);
+        
+        System.out.println(ergebnis);
+
+    }
+    
+    public static int addiere(int z1, int z2){
+        return z1+z2;
+    }
+
+}
