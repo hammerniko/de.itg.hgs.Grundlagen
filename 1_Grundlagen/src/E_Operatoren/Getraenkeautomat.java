038f7ca5f9808e341ee74106c03edcd4a471cfcc
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-02-20 12:16:00 +0100

    runden ergänzt in Getränkeautomat

--- 1_Grundlagen/src/E_Operatoren/Getraenkeautomat.java
+++ 1_Grundlagen/src/E_Operatoren/Getraenkeautomat.java
@@ -21,50 +21,60 @@ public class Getraenkeautomat {
 		anzahlMax = (int) (betrag / preis);
 		
 		do {
-			System.out.println("Anzahl:\t\t");
+			System.out.print("Anzahl:\t\t");
 			bestellt = s.nextInt();
         } while (bestellt>anzahlMax);
 		
 		
 		//RŸckgeld
-		kosten = bestellt * preis;
-		rueckgeld = betrag - kosten;
+		kosten = runden(bestellt * preis);
+		rueckgeld = runden(betrag - kosten);
 		rest=rueckgeld*100;	
 		
 		//MŸnzen
 		zweiEuro = (int) rest/200;
-		rest = rest%200;
-		einEuro = (int) rest/100;
-		rest = rest%100;
-		fuenfzigCent = (int) rest/50;
-		rest = rest%50;
-		zwanzigCent = (int) rest/20;
-		rest = rest%20;
-		zehnCent = (int) rest/10;
-		rest = rest%10;
-		fuenfCent = (int) rest/5;
-		rest = rest%5;
-		zweiCent = (int) rest/2;
-		rest = rest%2;
-		cent = (int) rest;
+		rest = runden(rest%200);
 		
+		einEuro = (int) rest/100;
+		rest = runden(rest%100);
+		
+		fuenfzigCent = (int) rest/50;
+		rest = runden(rest%50);
+		
+		zwanzigCent = (int) rest/20;
+		rest = runden(rest%20);
+		
+		zehnCent = (int) rest/10;
+		rest = runden(rest%10);
+		
+		fuenfCent = (int) rest/5;
+		rest = runden(rest%5);
+		
+		zweiCent = (int) rest/2;
+		rest = runden(rest%2);
+		
+		cent = (int) runden(rest);
 		
 		//Ausgabe
-		System.out.println("Betrag:\t\t"+betrag);
+		System.out.println("\nBetrag:\t\t"+betrag);
 		System.out.println("Anzahl GetrŠnke:"+bestellt);
 		System.out.println("Kosten:\t\t"+kosten);
 		System.out.println("RŸckgeld:\t"+rueckgeld);
 		System.out.println();
-		System.out.println("2 Euro:"+zweiEuro);
-		System.out.println("1 Euro:"+einEuro);
-		System.out.println("50 Cent:"+fuenfzigCent);
-		System.out.println("20 Cent:"+zwanzigCent);
-		System.out.println("10 Cent:"+zehnCent);
-		System.out.println("5 Cent:"+fuenfCent);
-		System.out.println("2 Cent:"+zweiCent);
-		System.out.println("1 Cent:"+cent);
+		System.out.println("2 Euro: \t"+zweiEuro);
+		System.out.println("1 Euro: \t"+einEuro);
+		System.out.println("50 Cent:\t"+fuenfzigCent);
+		System.out.println("20 Cent:\t"+zwanzigCent);
+		System.out.println("10 Cent:\t"+zehnCent);
+		System.out.println("5 Cent: \t"+fuenfCent);
+		System.out.println("2 Cent: \t"+zweiCent);
+		System.out.println("1 Cent: \t"+cent);
 		
 		
 		
+	}
+	
+	public static double runden(double zahl){
+		return Math.rint( zahl * 100 ) / 100.;
 	}
 }
