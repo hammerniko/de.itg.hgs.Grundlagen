13ad60f1056b6bbc32b3e7a4d7e59b9fcb3b1b33
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-02-28 14:19:56 +0100

    file modified

--- 1_Grundlagen/src/I2_MethodenUndGrafik/MeinPanel.java
+++ 1_Grundlagen/src/I2_MethodenUndGrafik/MeinPanel.java
@@ -1,29 +1,34 @@
 package I2_MethodenUndGrafik;
 
+
+import java.awt.Color;
 import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 
 public class MeinPanel extends JPanel {
 
-    
-    
-	@Override
+    @Override
     protected void paintComponent(Graphics g) {
-	    
-	    super.paintComponent(g);
-	    
-	    zeichneHaus(g,100,100);
-	    
+        super.paintComponent(g);
+        
+        zeichneHaus(g, 10, 10, 50, Color.BLUE);
+       
+        
     }
-	
-	public void zeichneHaus(Graphics g, int posX, int posY){
-		g.drawLine(200,posY,200,200);
-		g.drawOval(10, 10, 50, 70);
-		
-	
-	}
-	
+    
+    
+    public void zeichneHaus(Graphics g, int posX, int posY, int breite, Color color){
+        g.setColor(color);
+        g.drawRect(posX, posY, breite, breite);
+        
+        
+    }
+
+    
+    
+    
 	
 	
 	
