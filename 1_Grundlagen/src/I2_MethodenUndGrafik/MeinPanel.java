c07154c0e83266ce45fe0156b30d39dd289781c2
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-02-28 15:14:31 +0100

    rasen added

--- 1_Grundlagen/src/I2_MethodenUndGrafik/MeinPanel.java
+++ 1_Grundlagen/src/I2_MethodenUndGrafik/MeinPanel.java
@@ -1,35 +1,41 @@
 package I2_MethodenUndGrafik;
 
-
 import java.awt.Color;
 import java.awt.Graphics;
 
 import javax.swing.JPanel;
-
 
 public class MeinPanel extends JPanel {
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        
-        zeichneHaus(g, 10, 10, 50, Color.BLUE);
-       
-        
-    }
-    
-    
-    public void zeichneHaus(Graphics g, int posX, int posY, int breite, Color color){
-        g.setColor(color);
-        g.drawRect(posX, posY, breite, breite);
-        
-        
+
+        zeichneRasen(g, 50);
+
     }
 
-    
-    
-    
-	
-	
-	
+    public void zeichneRasen(Graphics g, int hoehe) {
+
+        g.setColor(Color.green);
+       for (int y = 5; y < 50; y=y+3) {
+            for (int i = 0; i < this.getWidth() - 1; i = i + 1) {
+                zeichneGrashalm(g, i, y);
+            }
+       }
+
+
+    }
+
+    public void zeichneGrashalm(Graphics g, int posX, int hoehe) {
+        int deltaFarbe = (int) (Math.random() * 50 + 1); 
+        int deltaX = (int) (Math.random() * 15 + 1);
+        int deltaHoehe = (int) (Math.random() * 13 + 1);
+        Color farbe = new Color(10+deltaFarbe,200+deltaFarbe,55-deltaFarbe);
+        g.setColor(farbe);
+        
+        g.drawLine(posX, this.getHeight() - hoehe, posX + deltaX,
+                this.getHeight() - hoehe - deltaHoehe);
+    }
+
 }
