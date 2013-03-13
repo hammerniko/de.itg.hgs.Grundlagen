062b0280a3bd228add945542a7187659b8df2786
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-03-13 05:47:36 +0100

    file modified

--- 1_Grundlagen/src/G_Schleifen/SchleifeWhile.java
+++ 1_Grundlagen/src/G_Schleifen/SchleifeWhile.java
@@ -3,27 +3,19 @@ package G_Schleifen;
 import java.util.Scanner;
 
 public class SchleifeWhile {
-
     
     public static void main(String[] args) {
              
         String passwort;
-        int a;
-        
-        
-        
-        
         Scanner s = new Scanner(System.in);
-       
         
         do {
           System.out.println("Passwort:");
           passwort = s.next();
-        } while (!passwort.equals("hallo"));
-
+        } while (!passwort.equals("12xz57f"));
         
-       
-        
+        System.out.println("Passwort akzeptiert.");
+      
     }
 
 }
