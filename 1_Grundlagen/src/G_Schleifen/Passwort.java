0f02fd1cb4ecae07bbc5dc42ec27299ac39839c1
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-03-19 20:11:22 +0100

    File added

--- 1_Grundlagen/src/G_Schleifen/Passwort.java
+++ 1_Grundlagen/src/G_Schleifen/Passwort.java
@@ -3,38 +3,29 @@ import java.util.Scanner;
 
 public class Passwort {
 	public static void main(String[] args) {
-		boolean istRechteingeschraenkt, istLoginKorrekt=false;
-		
+		boolean istRechteingeschraenkt, istLoginKorrekt = false;
 		String benutzername, passwort;
 		Scanner s = new Scanner(System.in);
 		do {
 			System.out.print("Benutzername:");
 			benutzername = s.next();
-			
 			System.out.print("Passwort:");
 			passwort = s.next();
-			
-			if(benutzername.equals("gast")){
-				if(passwort.equals("gast")){
-					istLoginKorrekt=true;
+			if (benutzername.equals("gast")) {
+				if (passwort.equals("gast")) {
+					istLoginKorrekt = true;
 				}
+			} else if (benutzername.equals("admin")) {
+				if (passwort.equals("toor")) {
+					istLoginKorrekt = true;
+				}
+			} else {
+				istLoginKorrekt = false;
+				System.out.println();
+				System.out.println("Falsches Passwort oder Benutzername");
+				System.out.println("------------------------------------");
 			}
-			else if(benutzername.equals("admin")) { 
-			    if(passwort.equals("toor")){ 
-			    		istLoginKorrekt=true;
-			
-			}
-			}
-			    else{
-			    	istLoginKorrekt=false;
-			    	System.out.println();
-			    	System.out.println("Falsches Passwort oder Benutzername");
-			    	System.out.println("------------------------------------");
-			    }
-			
-			
 		} while (!istLoginKorrekt);
-		
 		System.out.println("Passwort akzeptiert.");
 	}
 }
