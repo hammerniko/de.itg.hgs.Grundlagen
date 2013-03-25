bf8477493b698af271cb0e0685ac1c683c3cfcff
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-03-25 18:43:20 +0100

    files added

---
+++ 1_Grundlagen/src/G_Schleifen/Email.java
@@ -0,0 +1,29 @@
+package G_Schleifen;
+import java.util.Scanner;
+
+public class Email {
+	public static void main(String[] args) {
+		boolean istEmailKorrekt = false;
+		String email;
+		Scanner s = new Scanner(System.in);
+		
+		do {
+			System.out.println();
+			System.out.print("Email:");
+			email = s.next();
+			
+			if(email.contains("@") && (email.contains(".de") || email.contains(".com"))){
+				istEmailKorrekt=true;
+			}
+			else {
+				System.err.println("Fehler in der Email:"+email);
+				System.out.println("--------------------");
+			}
+					
+			
+			
+		} while (!istEmailKorrekt);
+		
+		System.out.println("Email akzeptiert.");
+	}
+}
---
+++ 1_Grundlagen/src/K_javadoc/Europa.java
@@ -0,0 +1,11 @@
+package K_javadoc;
+
+public class Europa {
+	/**
+	 * @
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
