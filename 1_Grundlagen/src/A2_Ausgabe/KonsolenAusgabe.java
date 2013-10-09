8d5d24d0b35e80c8b0faf748d50625fd97ccea66
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-10-09 14:51:57 +0200

    modified

--- 1_Grundlagen/src/A2_Ausgabe/KonsolenAusgabe.java
+++ 1_Grundlagen/src/A2_Ausgabe/KonsolenAusgabe.java
@@ -1,36 +1,53 @@
 package A2_Ausgabe;
 
 public class KonsolenAusgabe {
-  
-	
-	public static void main(String[] args) {
-    //Ausgabe von Zahlen
-     // System.out.print(234);
 
-//    //Ausgabe von Zeichen
-     //System.out.println("Mein Name ist N. Hammer"); 
-//		
-//    //Zahlen und Zeichen
-      //System.out.println("Mein Alter ist ");
-      //System.out.println(43);
-//		
-      //System.out.println("Mein Alter ist 43");
-//		
-//    //Rechnen innerhalb von System.out.print()
-      //System.out.println(3+5); //8
-      //System.out.println(3+"5"); //35
-      //System.out.println("x"+5); //x5
-      
-	    //System.out.println("Vorname"+"Nachname"); 
-     // System.out.println(1+2+"3"); 
-    //System.out.println("1"+(2+3)); 
-    //System.out.println(1+2+"3"+4); 
-//		
-//    //Zeilenumbruch mit Escape Sequenz
-    //System.out.println("Ich erzähle eine \neine neue Geschichte");
-//		
-//    //Tabulator mit Escape Sequenz
-    //System.out.println("Name\tVorname\t\tAdresse");
-    //System.out.println("Niko\tHammer\t\tHauptstrasse 11");
-   }
+    public static void main(String[] args) {
+        // Ausgabe von Zahlen
+        //System.out.print("234,45");
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Ausgabe von Zeichen
+        //System.out.println("Mein Name ist N. Hammer");
+        //
+        // Zahlen und Zeichen
+        //System.out.print("Mein Alter ist ");
+        //System.out.println(44);
+        
+        
+        
+        
+        //System.out.println("Mein Alter ist "+43);
+        
+        
+        
+        
+        
+        
+        // Rechnen innerhalb von System.out.print()
+        //System.out.println(3+5);
+         //System.out.println(3+"5");
+         //System.out.println("x"+5);
+
+         //System.out.println("Vorname"+" "+"Nachname");
+         //System.out.println(1+2+"3");
+         //System.out.println("1"+(2+3));
+         //System.out.println(1+2+"3"+(4+5));
+
+        // Zeilenumbruch mit Escape Sequenz
+         System.out.println("Ich erzähle eine \neine neue Geschichte");
+
+        // Tabulator mit Escape Sequenz
+        System.out.println("Name\tVorname\tAdresse");
+        System.out.println("Niko\tHammer\tHauptstrasse 11");
+    }
 }
