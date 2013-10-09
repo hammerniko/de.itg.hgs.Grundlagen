9716d7078faa609ca19c9d8a0ca661fd9dd1a876
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-10-09 12:09:04 +0200

    files added

---
+++ 1_Grundlagen/src/P3a_BidirektionaleAssoziationen1_n/Kind.java
@@ -0,0 +1,24 @@
+package P3a_BidirektionaleAssoziationen1_n;
+
+public class Kind extends Person{
+	
+	Mutter	mutter;
+	
+	public Kind(String name, String vorname){
+		super(name,vorname);
+	}
+
+	public void setMutter(Mutter m) {
+		mutter = m;
+		if (m != null && !m.getKinder().contains(this)){
+			m.addKind(this);
+		}
+	}
+
+	@Override
+    public String toString() {
+	    return super.toString()+ " Mutter=" + mutter.getVorname() + "]";
+    }
+	
+	
+}
