8f3323ee9fe69c91db05fc7dbee9348b21dac9c1
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-11-15 09:27:10 +0100

    vererbungsuebung added

--- 1_Grundlagen/src/A6_Operatoren/Operatoren.java
+++ 1_Grundlagen/src/A6_Operatoren/Operatoren.java
@@ -3,6 +3,7 @@ package A6_Operatoren;
 public class Operatoren {
 
 	public static void main(String[] args) {
+	//****************EINGABE********************
 		// Deklaration
 		String str_z1, str_z2;
 		String lsg_a, lsg_b, lsg_c;
@@ -12,20 +13,22 @@ public class Operatoren {
 		str_z1 = "1111";
 		str_z2 = "1001";
 
+     //**************VERARBEITUNG***********************
 		// Umwandeln in ganze Zahlen
 		z1 = Integer.parseInt(str_z1, 2);
 		z2 = Integer.parseInt(str_z2, 2);
 
 		// logisch Verknüpfen
-		a = z1 & z2;
-		b = z1 | z2;
-		c = z1 ^ z2;
+		a = z1 & z2; //9
+		b = z1 | z2; //15
+		c = z1 ^ z2; //6
 
-		// Umwandeln Binärzahl als String
+	// Umwandeln Binärzahl als String
 		lsg_a = Integer.toBinaryString(a);
 		lsg_b = Integer.toBinaryString(b);
 		lsg_c = Integer.toBinaryString(c);
 
+		//**************AUSGABE*****************************
 		System.out.println(lsg_a);
 		System.out.println(lsg_b);
 		System.out.println(lsg_c);
