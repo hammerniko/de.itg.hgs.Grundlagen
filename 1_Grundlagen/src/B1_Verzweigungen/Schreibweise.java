fa29390ca31c622e5affaee690a13905f3639d83
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-11-20 16:49:20 +0100

    file added

---
+++ 1_Grundlagen/src/B1_Verzweigungen/Schreibweise.java
@@ -0,0 +1,32 @@
+package B1_Verzweigungen;
+
+public class Schreibweise {
+
+    public static void main(String[] args) {
+        
+        boolean istPasswortGesetzt;
+        
+        
+        //..Passwortabfrage
+        
+        istPasswortGesetzt = true;
+        
+        //...
+        int a = 5;
+        String name = "Hammer";
+        name = name + " Nikolai";
+        System.out.println(name);
+        
+        if (name.equals("Hammer Nikolai")) {
+            System.out.println("Das ist wahr");
+        }
+        
+        else {
+            System.out.println("Das ist nicht wahr");
+        }
+        
+        
+        System.out.println("Prüfung beendet");
+    }
+
+}
