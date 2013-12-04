8e04fa9f8aced9a75b3a48f119b17355fb0b0c41
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-12-04 14:24:41 +0100

    file added, Paket Dreischichten Architektur entfernt

---
+++ 1_Grundlagen/src/C1_Schleifen/SchleifenGrundlagen.java
@@ -0,0 +1,37 @@
+package C1_Schleifen;
+
+public class SchleifenGrundlagen {
+
+    public static void main(String[] args) {
+        
+      
+        //zufallszahl
+        int zahl;
+        zahl = (int) (Math.random()*6 +1);
+        
+        System.out.println(zahl);
+        
+        
+        
+        
+        
+        //while Schleifen
+        
+//        boolean istAGroesserB=false;
+//        int a=1;
+//        int b=5000;
+//        int anzahl=0;
+//        
+//        while (a<b) {
+//            anzahl++;
+//            a=a*3+1;
+//            
+//            System.out.println("anzahl:"+anzahl+" a="+a);
+//        }
+//        
+//        
+        
+
+    }
+
+}
