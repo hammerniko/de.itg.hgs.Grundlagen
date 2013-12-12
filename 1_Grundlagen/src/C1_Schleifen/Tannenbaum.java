1b704e39216ff2f674774dadf37df396553e3773
Nikolai Hammer <hammer.nikolai@googlemail.com>
2013-12-12 11:12:40 +0100

    Tannebaum dokumentiert

--- 1_Grundlagen/src/C1_Schleifen/Tannenbaum.java
+++ 1_Grundlagen/src/C1_Schleifen/Tannenbaum.java
@@ -3,28 +3,41 @@ package C1_Schleifen;
 public class Tannenbaum {
 
     /**
-     * @param args
+     * Erzeuge eine Tannenbaumkrone aus "*"-Zeichen mit einer
+     * beliebigen Anzahl von Sternenreihen.
      */
     public static void main(String[] args) {
-        String luecke = "";
-        String stern = "*";
+    	
+    	//Initialisierung
+        String luecke;
+        String stern;
         String zeile = "";
         int anzahlSterne = 0;
+        int anzahlReihen=10;
 
-        for (int i = 0; i <= 10; i++) {
-            for (int j = 10 - i; j > 0; j--) {
+        //Erzeuge eine Anzahl von Reihen
+        for (int i = 0; i <= anzahlReihen; i++) {
+        	luecke = "";
+            stern = "*";
+        	
+        	
+        	//Erzeuge LŸcken 
+            for (int j = anzahlReihen - i; j > 0; j--) {
                 luecke = luecke + " ";
             }
+            
+            //Erzeuge Sterne
             anzahlSterne = i * 2;
-
             for (int k = 0; k < anzahlSterne; k++) {
                 stern = stern + "*";
             }
 
+            //Bilde eine Zeile aus LŸcken und Sternen
             zeile = luecke + stern;
+            
+            //Zeilenumbruch 
             System.out.println(zeile);
-            luecke = "";
-            stern = "*";
+            
 
         }
 
