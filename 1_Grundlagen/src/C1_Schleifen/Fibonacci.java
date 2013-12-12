57794902d7ea4992cdc1d194eeeeb47965279167
Hammer Nikolai <niko.hammer@hgs-singen.de>
2013-12-12 15:10:49 +0100

    Struktogramm Fibonacci added

--- 1_Grundlagen/src/C1_Schleifen/Fibonacci.java
+++ 1_Grundlagen/src/C1_Schleifen/Fibonacci.java
@@ -2,6 +2,7 @@ package C1_Schleifen;
 public class Fibonacci {
 	
 	public static void main(String[] args) {
+	    
 		int neuGeborene=1,einMonatAlt=0,zweiMonateUndAelter=0,anzKan;
 		
 		for (int monat = 0; monat < 16; monat++) {
