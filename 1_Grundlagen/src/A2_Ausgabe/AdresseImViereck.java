47047d8c0bef0a3a78979111ebbb73fe4dcb2bcb
nh <n.hammer@online.de>
2015-10-03 18:54:39 +0200

    Sonderzeichen entfernt

--- 1_Grundlagen/src/A2_Ausgabe/AdresseImViereck.java
+++ 1_Grundlagen/src/A2_Ausgabe/AdresseImViereck.java
@@ -6,7 +6,7 @@ public class AdresseImViereck {
     public static void main(String[] args) {
         System.out.println("-------------------------");
         System.out.println("|\tMeier Markus\t|");
-        System.out.println("|\tMaxStraﬂe 2 \t|");
+        System.out.println("|\tMaxStrasse 2 \t|");
         System.out.println("|\t            \t|");
         System.out.println("|\t78224 Singen\t|");
         System.out.println("-------------------------");
