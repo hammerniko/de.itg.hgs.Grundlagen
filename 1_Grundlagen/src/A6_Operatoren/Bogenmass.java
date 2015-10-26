93eebd3b71c748c9d21b31ef3fcab757ca1264b3
nh <n.hammer@online.de>
2015-10-26 20:20:12 +0100

    Bogenmass Uebung hinzugefuegt

---
+++ 1_Grundlagen/src/A6_Operatoren/Bogenmass.java
@@ -0,0 +1,27 @@
+package A6_Operatoren;
+
+public class Bogenmass {
+
+	public static void main(String[] args) {
+		
+		
+		//Winkel
+		double alpha = 45;
+		
+		//Umrechnung in Bogenmass
+		double bmAlpha = alpha * 2.0*Math.PI /360;
+		
+		//Winkelfunktion vom Bogenmass
+		double sin = Math.sin(bmAlpha);
+		System.out.println(sin);
+
+		
+		//und umgekehrt
+		double bm = Math.asin(sin);
+		double alphaBerechnet = bm *360 / (2*Math.PI);
+		System.out.println(alphaBerechnet);
+		
+		
+	}
+
+}
