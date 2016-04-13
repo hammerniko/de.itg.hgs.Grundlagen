509cbb025afff822203ccd6fb4ba0a102c75d719
hr <hammer.nikolai@googlemail.com>
2016-04-13 16:48:42 +0200

    highscore added

--- 1_Grundlagen/src/E1_Methoden/Hangman.java
+++ 1_Grundlagen/src/E1_Methoden/Hangman.java
@@ -3,24 +3,27 @@ package E1_Methoden;
 import java.util.Scanner;
 
 public class Hangman {
-
+	static int highscore = 0;
+	static long startZeit = System.currentTimeMillis();
+	static long endZeit,zwischenzeit, dauer;
+	
+	
 	public static void main(String[] args) {
-		//Varriable
-		String eingegebeneZeichen;
-		char buchstabe;
+		//Varriablen initialisieren
 		int anzahl = 0;
+		String eingegebeneZeichen="";
+		Scanner stream = new Scanner(System.in);
 		
-		//Wortliste
-		String wort[] = {"windsurfen","sandmann","mauseloch","sprintrennen","weihnachtsmann","feierabend","schule",};
+		//Wortliste initialisieren
+		String wortliste[] = {"windsurfen","sandmann","mauseloch","sprintrennen","weihnachtsmann","feierabend","schule","hirsch","looser","heino"};
 		
 		//Zufällig ein Wort aus der Liste bestimmen
-		String aktWort = (wort[gibIndex()]);
+		String aktWort = gibWort(wortliste);
+		//System.out.println(aktWort);
 		
 		//gefundener Buchstabe = 1, offener Buchstabe = 0
 		int[] zustand = new int[aktWort.length()];
-		
-		//Eingabestrom öffnen
-		Scanner s = new Scanner(System.in);
+			
 		
 		do {
 					
@@ -29,46 +32,88 @@ public class Hangman {
 			
 			//Buchstabe einlesen
 			anzahl++;
-			System.out.print(anzahl+".\nBuchstabe:");
-			eingegebeneZeichen = s.next();
+			eingegebeneZeichen = eingabeBuchstabe(anzahl, stream);
 			
 			
 			//Ändere den Zustand
-			zustand = setzeZustand(aktWort, zustand, eingegebeneZeichen);
+			zustand = setzeZustand(aktWort, zustand, eingegebeneZeichen, anzahl);
 			
 			
 		} while (!istWortVollstaendig(zustand));
 			
 		
-		System.out.println("Geschafft, Sie haben "+anzahl+" Versuche benötigt");
 		
+		setHighscore();
+		
+		
+		System.out.println("Geschafft, Sie haben "+anzahl+" Versuche benötigt");
+		System.out.println("Sie haben "+highscore+" Punkte erreicht");
 		
 
 	}
+
+	private static void bestimmeDauer() {
+		endZeit = System.currentTimeMillis();
+		dauer = (endZeit - startZeit)/1000;
+	}
+
+	private void init(){
+		
+	}
+
+
+
+	private static String eingabeBuchstabe(int anzahl, Scanner s) {
+		String eingegebeneZeichen;
+		System.out.print(anzahl+".\nBuchstabe:");
+		eingegebeneZeichen = s.next();
+		return eingegebeneZeichen;
+	}
+
+
+	private static String gibWort(String[] wortliste) {
+		return wortliste[gibIndex(wortliste.length)];
+	}
 	
 	
-	public static int[] setzeZustand(String aktWort, int[] zustand, String eingWort) {
+
+	public static int[] setzeZustand(String aktWort, int[] zustand, String eingabe, int anzahl) {
 		
 		//Wenn das eingegebene Wort stimmt
-		if(aktWort.equals(eingWort)){
+		if(aktWort.equals(eingabe)){
 			for (int i = 0; i < zustand.length; i++) {
 				zustand[i] = 1;
+				betimmeZwischenzeit();
+				setHighscore();
+							
 			}
 		}
 		
 		
 		//Wenn nur ein Zeichen eingegeben wurde
-		char b = eingWort.charAt(0);
+		char ersterBuchstabe = eingabe.charAt(0);
 		char[] zeichen = aktWort.toCharArray();
 		
 		for (int i = 0; i < zustand.length; i++) {
-			if(zeichen[i]==b){
+			if(zeichen[i]==ersterBuchstabe){
 				zustand[i]=1;
+				betimmeZwischenzeit();
+				setHighscore();
 			}
 		}
 		
 		return zustand;
 		
+	}
+
+	private static void betimmeZwischenzeit() {
+		zwischenzeit = System.currentTimeMillis();
+		dauer = (zwischenzeit - startZeit)/1000;
+	}
+
+	private static void setHighscore() {
+		bestimmeDauer();
+		highscore = highscore +( 10000 / (int) dauer);
 	}
 
 
@@ -81,9 +126,8 @@ public class Hangman {
 				System.out.print(aktWort.charAt(i)+" ");
 			}
 			
-			
 		}
-		System.out.println("\n");
+		System.out.println("Punkte:\n"+highscore);
 		
 	}
 
@@ -100,9 +144,9 @@ public class Hangman {
 	}
 	
 	
-	public static int gibIndex(){
-		int zufallszahl= (int) (Math.random()*4+1);
-		return zufallszahl-1;
+	public static int gibIndex(int anzahl){
+		int zufallszahl = (int) (Math.random()*anzahl);
+		return zufallszahl;
 	}
 
 }
