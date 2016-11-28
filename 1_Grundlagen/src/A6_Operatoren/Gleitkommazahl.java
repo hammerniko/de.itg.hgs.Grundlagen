2b9d3ba895815f48faeedd76183e3d56b361f34a
hr <hammer.nikolai@googlemail.com>
2016-11-28 16:19:46 +0100

    modified

--- 1_Grundlagen/src/A6_Operatoren/Gleitkommazahl.java
+++ 1_Grundlagen/src/A6_Operatoren/Gleitkommazahl.java
@@ -15,9 +15,13 @@ public class Gleitkommazahl {
 				double excess;
 				int vkDezimal;
 				double nkDezimal, z;
-				String vkBinaer, nkBinaer="";
+				String vkBinaer, nkBinaer="", fkZahl="", normierteFkZahl="";
+				int kommaNachLinksUm=0;
+				int pos; //Position innerhalb der Nachkommastellen
+				int erstePosMitEins = 0;
+				boolean wurdeEinsGesetzt = false;
 				
-				System.out.print("Kommazahl:\t\t\t");
+				System.out.print("Kommazahl: ");
 				dezimalzahl = s.nextDouble();
 				
 				//Verarbeitung
@@ -31,29 +35,57 @@ public class Gleitkommazahl {
 				
 				//Rechenwert für Nachkommastellen
 				z=nkDezimal;
+				pos=0;
 				
 				for (int i = 0; i < anzahlBitsMantisse; i++) {
 					
 					if(z*2 > 1){
 						nkBinaer = nkBinaer +"1";
 						z = z * 2 - 1; 
+						//Merke die Position der zuerst gesetzten Eins
+						if(!wurdeEinsGesetzt){
+							wurdeEinsGesetzt = true;
+							erstePosMitEins = pos;
+						}
 					}
 					else{
 						nkBinaer = nkBinaer +"0";
 						z= z * 2;
 					}
-		}
+					pos++;
+			
+				}
+			    //Festkommazahl
+				fkZahl = vkBinaer+","+nkBinaer;
+				
+				//normierte FK-Zahl
+				if(vkDezimal>0){
+					kommaNachLinksUm = vkBinaer.length()-1;
+					System.out.println(vkBinaer.substring(1,vkBinaer.length()));
+					
+				}
+				else{
+					//nachkommateil nach links verschieben bis eine 1 kommt.
+					kommaNachLinksUm = -(erstePosMitEins+1);
+				}
+				
+				vkBinaer = "1";
+			    
+		
 				
 				
 				
 				
 				//Ausgabe
-				System.out.println("Anzahl Bits: \t\t\t"+anzahlBits);
-				System.out.println("Anzahl Bits im Exponent:\t"+(int) anzahlBitsExponent);
-				System.out.println("Excess:\t\t\t\t"+(int) excess);
-				System.out.println("Vorkommateil Binaer:\t\t"+vkBinaer);
-				System.out.println("Nachkommateil Dez.:\t\t"+nkDezimal);
-				System.out.println("Nachkommateil binaer:"+nkBinaer);
+				System.out.println("Anzahl Bits: "+anzahlBits);
+				System.out.println("Anzahl Bits im Exponent: "+(int) anzahlBitsExponent);
+				System.out.println("1.) Excess: "+(int) excess);
+				System.out.println("2.) Vorkommateil Binaer: "+vkBinaer);
+				System.out.println("3.) Nachkommateil Dez.: "+nkDezimal);
+				System.out.println("4.) Nachkommateil binaer:"+nkBinaer);
+				System.out.println("5.) Festkommazahl: "+fkZahl);
+				System.out.println("6.) Kommaverschiebung um "+kommaNachLinksUm+" Stelle(n) nach links (neg. Werte nach rechts).");
+				
 				
 
 	}
