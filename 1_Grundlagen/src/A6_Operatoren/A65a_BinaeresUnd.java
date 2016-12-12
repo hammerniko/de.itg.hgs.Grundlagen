c0175c29a94e266d67dc40f8226042e1aa27ec25
hr <hammer.nikolai@googlemail.com>
2016-12-12 14:17:37 +0100

    modified

---
+++ 1_Grundlagen/src/A6_Operatoren/A65a_BinaeresUnd.java
@@ -0,0 +1,21 @@
+package A6_Operatoren;
+
+public class A65a_BinaeresUnd {
+
+	public static void main(String[] args) {
+		//Übung
+	    //Welchen Wert hat z4? Begründen Sie Ihr ergebnis.
+	    int a=13;    	//1101
+	    int b=3;     	//0011 
+	    
+	    //Verarbeitung
+	    int z = a&b;	//0001
+	    
+	    //ausgabe
+	    System.out.println("a = "+a);
+	    System.out.println("b = "+b);
+	    System.out.println("-------");
+	    System.out.println("z = "+z);
+	}
+	 
+}
