8d0e6d3dd86728334da1fa707e0cb4a09f5f7e02
hr <hammer.nikolai@googlemail.com>
2017-01-18 16:43:09 +0100

    modified

--- 1_Grundlagen/src/C1_Schleifen/C1_5_Passwort_Admin_Gast.java
+++ 1_Grundlagen/src/C1_Schleifen/C1_5_Passwort_Admin_Gast.java
@@ -1,50 +1,52 @@
 package C1_Schleifen;
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
-
 public class C1_5_Passwort_Admin_Gast {
 	public static void main(String[] args) {
-		boolean isAdmin=false;
+		boolean isAdmin = false;
 		boolean isLoginOk = false;
+		final int MAX_ANZ_ANMELDUNG = 3;
+		int anzAnmeldung = 0;
 		String user;
 		String password;
-		
+		String meldung = "";
 		Scanner s = new Scanner(System.in);
-		
+
 		do {
-			
-			//Eingabe
+			anzAnmeldung++;
+
 			System.out.print("Benutzername:");
 			user = s.next();
-			
+
 			System.out.print("Passwort:");
 			password = s.next();
-			
-			
-			//Verarbeitung
-			if (user.equals("gast")) {
-				if (password.equals("gast")) {
-					isLoginOk = true;
-					isAdmin=false;
-				}
-			} else if (user.equals("admin")) {
-				if (password.equals("toor")) {
-					isLoginOk = true;
-					isAdmin=true;
-				}
+
+			if (anzAnmeldung >= MAX_ANZ_ANMELDUNG) {
+				isAdmin = false;
+				isLoginOk = false;
+				meldung = "zu viele Fehlversuche";
+				break;
+			}
+
+			if (user.equals("gast") && password.equals("gast")) {
+				isLoginOk = true;
+				isAdmin = false;
+				meldung="Hello gast";
+
+			} else if (user.equals("admin") && password.equals("toor")) {
+				isLoginOk = true;
+				isAdmin = true;
+				meldung = "Hello admin";
 			} else {
 				isLoginOk = false;
 				System.out.println();
 				System.out.println("Falsches Passwort oder Benutzername");
 				System.out.println("------------------------------------");
+				System.out.println();
 			}
 		} while (!isLoginOk);
-		System.out.println("Passwort akzeptiert.");
-		
-		if(isAdmin){
-			System.out.println("Hello Admin");
-		}
-		
+
+		System.out.println(meldung+" isAdmin:"+isAdmin);
+		s.close();
 	}
 }
---
+++ 1_Grundlagen/src/C1_Schleifen/WhileEinfach.java
@@ -0,0 +1,17 @@
+package C1_Schleifen;
+
+public class WhileEinfach {
+
+	public static void main(String[] args) {
+		
+		int i=0;
+		
+		do {
+			System.out.println("hALLO");
+			
+		}while (i<0);
+		
+
+	}
+
+}
