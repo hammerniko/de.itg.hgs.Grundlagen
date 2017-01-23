a7a02d40c84e874180fcaeeef976217ae827da49
hr <hammer.nikolai@googlemail.com>
2017-01-23 15:00:56 +0100

    modified

--- 1_Grundlagen/src/J1_TSP/TSP.java
+++ 1_Grundlagen/src/J1_TSP/TSP.java
@@ -2,52 +2,122 @@ package J1_TSP;
 
 public class TSP {
 
+	static boolean debug = true;
 	static int[][] matrix = new int[5][5];
+	static int[][] penalties = new int[5][5];
 	static int min;
-	
+	static int indexMaxPenaltyReihe;
+	static int indexMaxPenaltySpalte;
 
 	public static void main(String[] args) {
 		setMatrix();
 		ausgabeMatrix();
-		
-		
+
 		minimiereAlleReihen();
+		minimiereAlleSpalten();
+
+		kalkulierePenaltiesMax();
+		ausgabePenalties();
 		
 		ausgabeMatrix();
 
 	}
 
+	private static void kalkulierePenaltiesMax() {
+		initPenalties();
+		
+		for (int reihe = 0; reihe < matrix.length; reihe++) {
+
+			for (int spalte = 0; spalte < matrix.length; spalte++) {
+				if (matrix[spalte][reihe] == 0) {
+
+					
+
+					
+				}
+			}
+		}
+
+	}
+
 	public static void minimiereAlleReihen() {
+		trace("Alle Reihen minimiert");
 		for (int i = 0; i < 5; i++) {
 			minimiereReihe(i);
+		}
+	}
+
+	public static void minimiereAlleSpalten() {
+		trace("Alle Spalten minimiert");
+		for (int j = 0; j < 5; j++) {
+			minimiereSpalte(j);
 		}
 	}
 
 	private static void minimiereReihe(int reihe) {
 		min = Integer.MAX_VALUE;
 		int indexMin;
-		
+
+		indexMin = getMinOfRow(reihe);
+
+		for (int j = 0; j < matrix.length; j++) {
+			if (matrix[j][reihe] != 999) {
+				matrix[j][reihe] = matrix[j][reihe] - min;
+			}
+		}
+
+	}
+
+	public static int getMinOfRow(int reihe) {
+		min = 999;
+		int indexMin = -1;
 		for (int j = 0; j < matrix.length; j++) {
 			if (matrix[j][reihe] < min) {
 				min = matrix[j][reihe];
 				indexMin = j;
 			}
 		}
-		
-		
-		for (int j = 0; j < matrix.length; j++) {
-			if(matrix[j][reihe]!=999){
-				matrix[j][reihe]=matrix[j][reihe]-min;
-			}
-		}
-		
-		
+		return indexMin;
 	}
 
-	
+	private static void minimiereSpalte(int spalte) {
+		min = Integer.MAX_VALUE;
+		int indexMin;
+
+		indexMin = getMinOfCol(spalte);
+
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[spalte][i] != 999) {
+				matrix[spalte][i] = matrix[spalte][i] - min;
+			}
+		}
+
+	}
+
+	public static int getMinOfCol(int spalte) {
+		min = 999;
+		int indexMin = -1;
+		for (int i = 0; i < matrix.length; i++) {
+			if (matrix[spalte][i] < min) {
+				min = matrix[spalte][i];
+				indexMin = i;
+			}
+		}
+		return indexMin;
+	}
+
+	private static void initPenalties() {
+		for (int i = 0; i < penalties.length; i++) {
+			System.out.println();
+			for (int j = 0; j < penalties.length; j++) {
+				penalties[i][j] = 0;
+			}
+		}
+
+	}
 
 	private static void ausgabeMatrix() {
-		
+
 		for (int i = 0; i < matrix.length; i++) {
 			System.out.println();
 			for (int j = 0; j < matrix.length; j++) {
@@ -56,6 +126,19 @@ public class TSP {
 				} else {
 					System.out.print(matrix[j][i] + "\t");
 				}
+			}
+		}
+		System.out.println("\n-------------------------------------");
+	}
+	
+	private static void ausgabePenalties() {
+
+		for (int i = 0; i < penalties.length; i++) {
+			System.out.println();
+			for (int j = 0; j < penalties.length; j++) {
+				
+					System.out.print(penalties[j][i] + "\t");
+				
 			}
 		}
 		System.out.println("\n-------------------------------------");
@@ -96,6 +179,12 @@ public class TSP {
 		matrix[2][4] = 8;
 		matrix[3][4] = 7;
 		matrix[4][4] = 999;
+	}
+
+	public static void trace(String meldung) {
+		if (debug) {
+			System.out.println(meldung);
+		}
 	}
 
 }
