5147a690451a8c26f6bd2afcac37da1f9479211c
nh <n.hammer@online.de>
2017-01-24 23:23:00 +0100

    Kommentare ergaenzt

--- 1_Grundlagen/src/J1_TSP/TSP.java
+++ 1_Grundlagen/src/J1_TSP/TSP.java
@@ -4,10 +4,18 @@ import java.util.Arrays;
 
 import com.sun.xml.internal.bind.v2.runtime.unmarshaller.XsiNilLoader.Array;
 
+/**
+ * Das Programm berechnet nach dem Travel Salesman Problem - Algorithmus ein
+ * Array mit den Daten(Entfernung) von 5 Orten (Knoten)
+ * 
+ * @author Nikolai Hammer
+ *
+ */
 public class TSP {
 
-	static String[] buchstaben = {"A","B","C","D","E","F","G","H"};
+	static String[] buchstaben = { "A", "B", "C", "D", "E", "F", "G", "H" };
 	static final int ANZAHL = 5;
+	static final int DASH = 999;
 	static boolean debug = false;
 	static int[][] matrix = new int[ANZAHL][ANZAHL];
 	static int[][] matrixCopy = new int[ANZAHL][ANZAHL];
@@ -23,32 +31,36 @@ public class TSP {
 		matrixCopy = arrayCopy(matrix);
 		ausgabeArray(matrix);
 
-		//TSP Algorithmus
-		for (int i = 0; i < ANZAHL; i++) {	
-		
-		// Minimieren
-		trace("Minimieren der Matrix");
-		minimiereAlleReihen();
-		minimiereAlleSpalten();
-		//ausgabeArray(matrix);
+		// ********************************** TSP Algorithmus
+		for (int i = 0; i < ANZAHL; i++) {
 
-		// Kalkulieren
-		trace("Kalkulieren der Penalties");
-		kalkulierePenalties();
-		//ausgabeArray(penalties);
+			// Minimieren
+			trace("Minimieren der Matrix");
+			minimiereAlleReihen();
+			minimiereAlleSpalten();
+			// ausgabeArray(matrix);
 
-		// Reduzieren der Matrix
-		trace("Reduzieren der Matrix");
-		reduziereMatrix();		
-		
+			// Kalkulieren
+			trace("Kalkulieren der Penalties");
+			kalkulierePenalties();
+			// ausgabeArray(penalties);
+
+			// Reduzieren der Matrix
+			trace("Reduzieren der Matrix");
+			reduziereMatrix();
+
 		}
-		
+
 		System.out.println("Distanz:" + distanz);
-		System.out.println("Loesung:\n"+loesung);
-		
-		
+		System.out.println("Loesung:" + loesung);
+
 	}
 
+	/**
+	 * Kopiert ein Array Wert fuer Wert und gibt die Kopie zurueck
+	 * @param array
+	 * @return
+	 */
 	private static int[][] arrayCopy(int[][] array) {
 
 		int[][] copiedArray = new int[array.length][array[0].length];
@@ -61,17 +73,17 @@ public class TSP {
 		return copiedArray;
 	}
 
-	
-	// Wenn eine maximale Penalty gefunden wurde
-	// Wird die Matrix an dieser Stelle reduziert,
-	// indem diese Reihe und Spalte gelöscht wird.
-	// Gleichzeitig ist dies eine gefundene Beziehung
-	// zwischen 2 Knoten
+	/** Wenn eine maximale Penalty gefunden wurde
+	*   Wird die Matrix an dieser Stelle reduziert,
+	*   indem diese Reihe und Spalte gelöscht wird.
+	*   Gleichzeitig ist dies eine gefundene Beziehung
+	*   zwischen 2 Knoten*/
 	private static void reduziereMatrix() {
 
+		//Initialisiere Maximalen Penaltywert
 		int max = 0;
 
-		// Merker
+		//Initialisiere Merker für Zeile und Spalte des max. Penalty-Werts
 		int colMaxPanelty = -1;
 		int rowMaxPanelty = -1;
 
@@ -80,50 +92,78 @@ public class TSP {
 		for (int reihe = 0; reihe < penalties.length; reihe++) {
 			for (int spalte = 0; spalte < penalties.length; spalte++) {
 				if (penalties[reihe][spalte] > max) {
+					
+					//maximaler Penaltywert wird gespeichert
+					//sobald ein groesserer Wert gefunden wurde
 					max = penalties[reihe][spalte];
+					
+					//Zeile und Spalte mit max-Penalty merken
 					colMaxPanelty = spalte;
 					rowMaxPanelty = reihe;
 				}
 			}
 		}
 
-		// setLoesung
-		setLoesung(colMaxPanelty, rowMaxPanelty);
+		//Die gefundenen Max-Werte stellen eine Loesung dar
+		//in der Form von Reihe nach Spalte (Festlegung)
+		addLoesung(colMaxPanelty, rowMaxPanelty);
 
-		// setDistance
-		setDistance(colMaxPanelty, rowMaxPanelty);
+		//Die Distanz der gefundenen Loesung wird 
+		//in der Variablen distance aufaddiert.
+		addDistance(colMaxPanelty, rowMaxPanelty);
 
 		// Streiche gefundene Loesung durch setzen von
-		// unendlich Werten (999)
+		// unendlich Werten in der gesamten Zeile und Spalte (DASH=999)
 		for (int i = 0; i < matrix.length; i++) {
-			matrix[i][colMaxPanelty]=999;
-			matrix[rowMaxPanelty][i]=999;
+			matrix[i][colMaxPanelty] = DASH;
+			matrix[rowMaxPanelty][i] = DASH;
 		}
-		
-		//Setze unendlich Wert auf den Rückweg (reihe und spalte vertauscht)
-		//der gefundenen Lösung, da ein Ort nur einmal 
-		//besucht werden darf
-		matrix[colMaxPanelty][rowMaxPanelty]=999;
-		
+
+		// Setze unendlich Wert auf den Rückweg (reihe und spalte vertauscht)
+		// der gefundenen Lösung, da ein Ort nur einmal
+		// besucht werden darf
+		matrix[colMaxPanelty][rowMaxPanelty] = DASH;
 
 	}
 
-	private static void setDistance(int colMaxPanelty, int rowMaxPanelty) {
+	/**
+	 * Addiert die Distanz zu einer bereits ermittelten Distanz
+	 * in der Variablen distanz.
+	 * 
+	 * @param colMaxPanelty
+	 * @param rowMaxPanelty
+	 */
+	private static void addDistance(int colMaxPanelty, int rowMaxPanelty) {
 		distanz = distanz + matrixCopy[rowMaxPanelty][colMaxPanelty];
-		
+
 	}
 
-	private static void setLoesung(int colMaxPanelty, int rowMaxPanelty) {		
-		loesung = loesung + buchstaben[rowMaxPanelty] + "->" + buchstaben[colMaxPanelty] +"\n";
+	/**
+	 * Fuegt einen String hinzu welcher eine Teilloesung darstellt.
+	 * 
+	 * @param colMaxPanelty
+	 * @param rowMaxPanelty
+	 */
+	private static void addLoesung(int colMaxPanelty, int rowMaxPanelty) {
+
+		loesung = loesung + buchstaben[rowMaxPanelty] + "->"
+				+ buchstaben[colMaxPanelty] + " ";
 		trace("Loesung:" + loesung);
 	}
 
+	/**
+	 * initialisiert und berechnet die Penalties aller Nullen der minimierten Matrix
+	 * und speichert das Ergebnis im Array penalties[][]
+	 */
 	private static void kalkulierePenalties() {
 		initPenalties();
 		kalkulierePenaltyReihen();
 		kalkulierePenaltySpalten();
 	}
 
+	/**
+	 * berechnet die Penalties der Spalten
+	 */
 	private static void kalkulierePenaltySpalten() {
 		// Merker
 		int min1;
@@ -133,9 +173,9 @@ public class TSP {
 		for (int spalte = 0; spalte < matrix.length; spalte++) {
 
 			// Merker für jede Reihe neu initialisieren
-			min1 = 999;
+			min1 = DASH;
 			reiheMitMin1 = -1;
-			min2 = 999;
+			min2 = DASH;
 
 			// Prüfe reihe
 			// Suche erstes Minimum und merke die Stelle und Wert
@@ -171,6 +211,9 @@ public class TSP {
 
 	}
 
+	/**
+	 * berechnet die Penalties der Reihen
+	 */
 	private static void kalkulierePenaltyReihen() {
 		// Merker
 		int min1;
@@ -180,9 +223,9 @@ public class TSP {
 		for (int reihe = 0; reihe < matrix.length; reihe++) {
 
 			// Merker für jede Reihe neu initialisieren
-			min1 = 999;
+			min1 = DASH;
 			spalteMitMin1 = -1;
-			min2 = 999;
+			min2 = DASH;
 
 			// Prüfe reihe
 			// Suche erstes Minimum und merke die Stelle und Wert
@@ -215,6 +258,9 @@ public class TSP {
 		}
 	}
 
+	/**
+	 * minimiert alle Reihen
+	 */
 	private static void minimiereAlleReihen() {
 		trace("-Alle Reihen minimiert");
 		for (int i = 0; i < 5; i++) {
@@ -222,6 +268,9 @@ public class TSP {
 		}
 	}
 
+	/**
+	 * minimiert alle Spalten
+	 */
 	private static void minimiereAlleSpalten() {
 		trace("-Alle Spalten minimiert");
 		for (int j = 0; j < 5; j++) {
@@ -229,20 +278,29 @@ public class TSP {
 		}
 	}
 
+	/**
+	 * Sucht in der reihe das minimum und zieht von
+	 * allen anderen Werten der reihe dieses Minimum ab.
+	 * Dashes werden ignoriert.
+	 * @param reihe
+	 */
 	private static void minimiereReihe(int reihe) {
 		min = Integer.MAX_VALUE;
-		int indexMin;
-
-		indexMin = getMinOfRow(reihe);
+		int indexMin = getMinOfRow(reihe);
 
 		for (int j = 0; j < matrix.length; j++) {
-			if (matrix[j][reihe] != 999) {
+			if (matrix[j][reihe] != DASH) {
 				matrix[j][reihe] = matrix[j][reihe] - min;
 			}
 		}
 
 	}
 
+	/**
+	 * gibt den Index mit dem Minimum einer Reihe zurueck
+	 * @param reihe
+	 * @return
+	 */
 	private static int getMinOfRow(int reihe) {
 		min = 999;
 		int indexMin = -1;
@@ -255,6 +313,11 @@ public class TSP {
 		return indexMin;
 	}
 
+	/**
+	 * Sucht das Minimum einer Spalte und zieht von allen anderen
+	 * Werten der Spalte dieses Minimum ab. Dashes werden ignoriert.
+	 * @param spalte
+	 */
 	private static void minimiereSpalte(int spalte) {
 		min = Integer.MAX_VALUE;
 		int indexMin;
@@ -269,6 +332,12 @@ public class TSP {
 
 	}
 
+	/**
+	 * Gibt den Index der Spalte mit dem Minimum zurueck.
+	 * 
+	 * @param spalte
+	 * @return
+	 */
 	private static int getMinOfCol(int spalte) {
 		min = 999;
 		int indexMin = -1;
@@ -281,6 +350,9 @@ public class TSP {
 		return indexMin;
 	}
 
+	/**
+	 * Setzt alle Penaltywert auf 0
+	 */
 	private static void initPenalties() {
 		for (int i = 0; i < penalties.length; i++) {
 			for (int j = 0; j < penalties.length; j++) {
@@ -290,31 +362,39 @@ public class TSP {
 
 	}
 
+	/**
+	 * Gibt ein Array mit zugehoerigen Knotenbeschriftung
+	 * auf der Konsole aus.
+	 * @param array
+	 */
 	private static void ausgabeArray(int[][] array) {
 
-		
 		for (int i = 0; i < array.length; i++) {
-			System.out.print("\t"+buchstaben[i]);
+			System.out.print("\t" + buchstaben[i]);
 		}
-		
+
 		for (int i = 0; i < array.length; i++) {
 			System.out.println();
-			System.out.print(buchstaben[i]+" ");
+			System.out.print(buchstaben[i] + " ");
 			for (int j = 0; j < array.length; j++) {
-				
+
 				if (array[j][i] == 999) {
 					System.out.print("\t-");
 				} else {
-					System.out.print("\t"+array[j][i] );
+					System.out.print("\t" + array[j][i]);
 				}
 			}
 		}
 		System.out.println("\n------------------------------------------");
 	}
 
+	/**
+	 * Setzt eine Beispielmatrix. Die Anzahld er Zeilen und Spalten
+	 * muss mit der Konstanten ANZAHL uebereinstimmen
+	 */
 	public static void setMatrix() {
 		// 1.Reihe
-		matrix[0][0] = 999; // steht f�r -
+		matrix[0][0] = DASH; 
 		matrix[1][0] = 10;
 		matrix[2][0] = 8;
 		matrix[3][0] = 2;
@@ -322,7 +402,7 @@ public class TSP {
 
 		// 2.Reihe
 		matrix[0][1] = 10;
-		matrix[1][1] = 999;
+		matrix[1][1] = DASH;
 		matrix[2][1] = 5;
 		matrix[3][1] = 4;
 		matrix[4][1] = 3;
@@ -330,7 +410,7 @@ public class TSP {
 		// 3.Reihe
 		matrix[0][2] = 8;
 		matrix[1][2] = 5;
-		matrix[2][2] = 999;
+		matrix[2][2] = DASH;
 		matrix[3][2] = 6;
 		matrix[4][2] = 2;
 
@@ -338,7 +418,7 @@ public class TSP {
 		matrix[0][3] = 2;
 		matrix[1][3] = 4;
 		matrix[2][3] = 6;
-		matrix[3][3] = 999;
+		matrix[3][3] = DASH;
 		matrix[4][3] = 3;
 
 		// 5.Reihe
@@ -346,7 +426,7 @@ public class TSP {
 		matrix[1][4] = 3;
 		matrix[2][4] = 2;
 		matrix[3][4] = 3;
-		matrix[4][4] = 999;
+		matrix[4][4] = DASH;
 	}
 
 	public static void trace(String meldung) {
