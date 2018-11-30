3e8c6c7c5ecc244a477169b6cd218383c6ea1545
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2018-11-30 15:18:05 +0100

    Struktogramm added

--- 1_Grundlagen/src/B1_Verzweigungen/B1_6_QuadratischeGleichung.java
+++ 1_Grundlagen/src/B1_Verzweigungen/B1_6_QuadratischeGleichung.java
@@ -5,55 +5,98 @@ import java.util.Scanner;
 
 public class B1_6_QuadratischeGleichung {
 
-    public static void main(String[] args) {
-        //***************Eingabe
-        String ergebnis = "";
-        double a,b,c,d,x1,x2,dis;
+	public static void main(String[] args) {
+		// ***************Eingabe
+		String ergebnis = "";
+		double a, b, c, d, x1, x2, dis;
+		String gleichung, str_a, str_b, str_c, str_d, str_dis;
+
+		Scanner s = new Scanner(System.in);
+
+		System.out.println("Bitte geben Sie eine quadratische Gleichung");
+		System.out.println("der Form ax^2+bx+c=d ein:");
+
+		System.out.print("a:\t");
+		a = s.nextDouble();
+		System.out.print("b:\t");
+		b = s.nextDouble();
+		System.out.print("c:\t");
+		c = s.nextDouble();
+		System.out.print("d:\t");
+		d = s.nextDouble();
+
+		// ******************VERARBEITUNG
+		System.out.println("\nIhre Gleichung:\t\t\t" + ausgabeGleichung(a, b, c, d));
+
+		// Nullform
+		c = c - d;
+		d = 0;
+		System.out.println("Ihre Gleichung in Nullform:\t" + ausgabeGleichung(a, b, c, d));
+
+		// Normalform
+		b = b / a;
+		c = c / a;
+		a = 1;
+		System.out.println("Ihre Gleichung in Normalform:\t" + ausgabeGleichung(a, b, c, d));
+
+		// Diskriminante
+		dis = (b * b / 4) - c;
+		
+		
+		if (dis < 0) {
+			str_dis="Die Dsikriminante ist < 0 -> Es gibt keine Lösung!";
+			ergebnis = "Es gibt keine Loesung";
+		} else if (dis == 0) {
+			str_dis="Die Diskriminante ist = 0 -> Es gibt eine Lösung!";
+			x1 = -b / 2;
+			ergebnis = "x = " + x1;
+		} else {
+			str_dis="Die Diskriminante ist > 0 -> Es gibt zwei Lösungen!";
+			x1 = -b / 2 + Math.sqrt(dis);
+			x2 = -b / 2 - Math.sqrt(dis);
+			ergebnis = "x1 = " + x1 + " \t x2= " + x2;
+		}
+
+		// ********************AUSGABE
+		System.out.println("\n"+str_dis);
+		System.out.println("\n" + ergebnis);
+	}
+
+	private static String ausgabeGleichung(double a, double b, double c, double d) {
+		String gleichung;
+		String str_a;
+		String str_b;
+		String str_c;
+		//Kontrollausgabe der Gleichung
+        //a
+		if(Math.abs(a)==1){
+			str_a = "\tx²\t";
+		}
+		else {
+			str_a = "\t"+a+"x²\t";
+		}
         
-        Scanner s = new Scanner(System.in);
-        
-        System.out.println("Bitte geben Sie eine quadratische Gleichung");
-        System.out.println("der Form ax^2+bx+c=d ein:");
-        
-        
-        System.out.print("a:\t");
-        a = s.nextDouble();
-       System.out.print("b:\t");
-        b = s.nextDouble();
-        System.out.print("c:\t");
-        c = s.nextDouble();
-        System.out.print("d:\t");
-        d = s.nextDouble();
-           
-        //******************VERARBEITUNG
-        
-        //Nullform
-        c=c-d;
-        d=0;
-        
-        //Normalform
-        b=b/a;
-        c=c/a;
-        a=1;
-        
-        //Diskriminante
-        dis = (b*b/4)-c;
-        
-        if(dis<0){
-            ergebnis = "Es gibt keine Loesung";
+        //b
+        if(b>=0) {
+        	str_b =" +\t"+b+"x "; 
         }
-        else if(dis==0) {
-            x1=-b/2;
-            ergebnis = "x = "+x1;
+        else if(Math.abs(b)==1) {
+        	str_b=b+"x";
         }
-        else{
-            x1 = -b/2 + Math.sqrt(dis);
-            x2 = -b/2 - Math.sqrt(dis);
-            ergebnis = "x1 = "+x1+" \t x2= "+x2;
+        else {
+        	str_b=b+"x";
         }
         
+        //c
+        if(c>=0) {
+        	str_c =" +\t"+c; 
+        }
+        else {
+        	str_c="\t"+c;
+        }
         
-        //********************AUSGABE
-        System.out.println(ergebnis);
-    }
+        //gleichung
+        gleichung = str_a+str_b+str_c+" = "+d;
+		return gleichung;
+	}
 }
