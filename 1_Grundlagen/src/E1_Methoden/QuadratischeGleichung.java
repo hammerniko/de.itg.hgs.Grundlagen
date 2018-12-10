6cc72381f59b8a3aeba8f4d9b817feab1ca35de6
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2018-12-10 16:47:14 +0100

    modified

--- 1_Grundlagen/src/E1_Methoden/QuadratischeGleichung.java
+++ 1_Grundlagen/src/E1_Methoden/QuadratischeGleichung.java
@@ -6,61 +6,56 @@ import java.util.Scanner;
 //x1|x2 = -p/2+- Wurzel ( (p/2)^2-q)
 //Diskriminante = (p/2)^2-q
 
-//Diskriminate  >0 -> 2 Lšsungen
-//Diskriminante <0 -> keine Lšsung
-//diskriminante =0 -> 1 Lšsung
+//Diskriminate  >0 -> 2 Loesung
+//Diskriminante <0 -> keine Loesung
+//diskriminante =0 -> 1 Loesung
 
 public class QuadratischeGleichung {
-     
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        // Eingabe
-        String ergebnis;
-   
-        // Verarbeitung
-        ergebnis = loeseQuadratischeGleichung(1, 2, 2, 1);
+		// Eingabe
+		String ergebnis;
 
-        // Ausgabe
-        System.out.println(ergebnis);
-        
-        
-    }
-    
-    
-    public static String loeseQuadratischeGleichung(double a, double b, double c, double d) {
-        double x1;
-        double x2;
-        double diskriminante;
-        String loesung;
+		// Verarbeitung
+		ergebnis = loeseQuadratischeGleichung(1, 2, 2, 1);
 
-        // bsp: 7x^2+3x-17=5
-        // wird umgeformt in 7x^2+3x-22=0
-        c = c - d;
-        d = 0;
-       
+		// Ausgabe
+		System.out.println(ergebnis);
 
-        // x^2 + 3/7x -22/7 = 0
-        b = b / a;
-        c = c / a;
-        a = 1;
+	}
 
-        // Diskriminante
-        diskriminante = (b * b / 4) - c;
+	public static String loeseQuadratischeGleichung(double a, double b, double c, double d) {
+		double x1;
+		double x2;
+		double diskriminante;
+		String loesung;
 
-        if (diskriminante > 0) {
-            x1 = -b / 2 + Math.sqrt(diskriminante);
-            x2 = -b / 2 - Math.sqrt(diskriminante);
-            loesung = "x1 = " + x1 + "\nx2 = " + x2;
-        } else if (diskriminante == 0) {
-            x1 = -b / 2;
-            loesung = "x = " + x1;
-        } else {
-            loesung = "Es gibt keine Lšsung";
-        }
+		// bsp: 7x^2+3x-17=5
+		// wird umgeformt in 7x^2+3x-22=0
+		c = c - d;
+		d = 0;
 
-        return loesung;
-    }
+		// x^2 + 3/7x -22/7 = 0
+		b = b / a;
+		c = c / a;
+		a = 1;
 
-   
+		// Diskriminante
+		diskriminante = (b * b / 4) - c;
+
+		if (diskriminante > 0) {
+			x1 = -b / 2 + Math.sqrt(diskriminante);
+			x2 = -b / 2 - Math.sqrt(diskriminante);
+			loesung = "x1 = " + x1 + "\nx2 = " + x2;
+		} else if (diskriminante == 0) {
+			x1 = -b / 2;
+			loesung = "x = " + x1;
+		} else {
+			loesung = "Es gibt keine Lšsung";
+		}
+
+		return loesung;
+	}
+
 }
