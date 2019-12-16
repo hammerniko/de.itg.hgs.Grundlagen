75def4bc117ebe65214c0dd95bd6bd806a857d8c
n.hammer <n.hammer@online.de>
2019-12-16 09:52:25 +0100

    modified

--- 1_Grundlagen/src/O1_GuiBinaerDezimal/Gui.java
+++ 1_Grundlagen/src/O1_GuiBinaerDezimal/Gui.java
@@ -2,20 +2,17 @@ package O1_GuiBinaerDezimal;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
-import java.awt.GridLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import javax.swing.border.EmptyBorder;
 
 public class Gui extends JFrame {
 
