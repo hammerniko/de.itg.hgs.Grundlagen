3ba925f1b900cf66a2dd293ccfc6698f5e1f404e
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-02-17 09:11:03 +0100

    file added

---
+++ 1_Grundlagen/src/B1_Verzweigungen/B1_1EinfacheVerzweigungSyntax.java
@@ -0,0 +1,52 @@
+package B1_Verzweigungen;
+
+public class B1_1EinfacheVerzweigungSyntax {
+
+	public static void main(String[] args) {
+		
+		
+		boolean istEsRegenerisch;
+		
+		istEsRegenerisch=true;
+		
+		
+		//Einfache Verzweigung
+		if (istEsRegenerisch) {
+			System.out.println("Bedinung erfüllt");
+		}
+		
+		
+		//Zweifach Verzweigung
+		if (istEsRegenerisch) {
+			
+		} else {
+
+		}
+		
+		
+		
+		//MehrfachVerzweigung
+		if (istEsRegenerisch) {
+			
+		} 
+		else if (istEsRegenerisch) {
+			
+		}
+		
+		else {
+
+		}
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
