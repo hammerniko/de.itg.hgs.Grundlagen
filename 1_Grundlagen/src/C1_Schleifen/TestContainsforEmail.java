9f6f17272f3b0026041d81d7053e57e7ef4e45f2
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-02-17 09:10:15 +0100

    testContains added

---
+++ 1_Grundlagen/src/C1_Schleifen/TestContainsforEmail.java
@@ -0,0 +1,18 @@
+package C1_Schleifen;
+
+public class TestContainsforEmail {
+	
+	public static void main(String[] args) {
+		
+		String name = "Meier";
+		
+		if(name.contains("ei")) {
+			System.out.println("Der Name enthält ein ei");
+		}
+		else {
+			System.out.println("Der Name enthält kein ei");
+		}
+		
+	}
+
+}
