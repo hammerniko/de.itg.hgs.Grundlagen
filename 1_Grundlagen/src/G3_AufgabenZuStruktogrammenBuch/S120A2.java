7c1c037e357171e0a28f140a00df192bb26b0862
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-06-15 09:21:20 +0200

    files added

---
+++ 1_Grundlagen/src/G3_AufgabenZuStruktogrammenBuch/S120A2.java
@@ -0,0 +1,50 @@
+package G3_AufgabenZuStruktogrammenBuch;
+
+public class S120A2 {
+	
+	//Statisches Feld, da der Spielstand 
+	//von allen Methoden benoetigt wird
+	static int[][] spielstand;
+	
+	
+	public static void main(String[] args) {
+		
+		//Spielstandfeld erstellen
+		spielstand = new int[3][3];
+		
+		//Spielstandfeld initialisieren
+		initSpielstand();
+		
+		//Spielstandfeld ausgeben
+		initSpielstand();
+	}
+	
+	
+	private static void initSpielstand() {
+		//hier beginnt Aufgabe 2
+		//spielstand[0][0]= 0;
+		//spielstand[0][1]= 0;
+		//spielstand[0][2]= 0;
+		//spielstand[1][0]= 0;
+		//spielstand[1][1]= 0;
+		//spielstand[1][2]= 0;
+		//spielstand[2][0]= 0;
+		//spielstand[2][1]= 0;
+		//spielstand[2][2]= 0;
+		
+		for (int zeile = 0; zeile <= 2; zeile++) {
+			for (int spalte = 0; spalte <= 2; spalte++) {
+				spielstand[zeile][spalte] = 0;
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ 1_Grundlagen/src/G3_AufgabenZuStruktogrammenBuch/S120A3.java
@@ -0,0 +1,33 @@
+package G3_AufgabenZuStruktogrammenBuch;
+
+public class S120A3 {
+	
+	public static void main(String[] args) {
+		
+		//Einfacher 6-stelliger Beispielcode
+		int zifferncode = 123456;
+				
+		//VERARBEITUNG
+		int ersteZiffer = gibErsteZiffer(zifferncode);
+		int letzteZiffer = gibLetzteZiffer(zifferncode);
+				
+		//AUSGABE
+		System.out.println(ersteZiffer);
+		System.out.println(letzteZiffer);
+				
+	}
+
+	
+	
+	
+	private static int gibLetzteZiffer(int zifferncode) {
+		// hier beginnt Aufgabe 3 Teil 1
+		return zifferncode%100;
+	}
+
+	private static int gibErsteZiffer(int zifferncode) {
+		// hier beginnt Aufgabe 3 Teil 2
+		return zifferncode/10000;
+	}
+
+}
