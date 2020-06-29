2ac8cb1e067e8dfcbf670724fe0a96079e9d04b5
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2020-06-29 08:48:43 +0200

    Functions added

--- 1_Grundlagen/src/G4_AufgabenmitDatum/StringInDatum.java
+++ 1_Grundlagen/src/G4_AufgabenmitDatum/StringInDatum.java
@@ -1,5 +1,8 @@
 package G4_AufgabenmitDatum;
 import java.util.*;
+
+import D1_Arrays.Getraenkeautomat;
+
 import java.text.*;
 
 
@@ -7,15 +10,27 @@ public class StringInDatum {
 
 	public static void main(String[] args) throws ParseException {
 		
-		SimpleDateFormat formatter = new SimpleDateFormat("dd.MMM.yyyy");
+		
         String dateInString = "7.Jun.2013";
         
-		Date date = formatter.parse(dateInString);
-        System.out.println(date);
-        System.out.println(formatter.format(date));
+        Date datum = getDate(dateInString);
         
-        
+        printDate(datum);
+       
 
+	}
+	
+	public static Date getDate(String strDatum) throws ParseException {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd.MMM.yyyy");
+		Date date = formatter.parse(strDatum);
+      
+        return date;
+        
+	}
+	
+	public static void printDate(Date datum) {
+		SimpleDateFormat formatter = new SimpleDateFormat("dd.MMM.yyyy");
+		System.out.println(formatter.format(datum));
 	}
 
 }
