5876d4959f00dea7642cfcefd2e110c4c392387c
n.hammer <n.hammer@online.de>
2020-10-19 15:23:05 +0200

    Serialize Beispiel added

--- 1_Grundlagen/src/P_Seriealisieren/TestSerializeObject.java
+++ 1_Grundlagen/src/P_Seriealisieren/TestSerializeObject.java
@@ -1,13 +1,47 @@
 package P_Seriealisieren;
 
-public class TestSerializeObject {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-	public TestSerializeObject() {
-		// TODO Auto-generated constructor stub
+public class TestSerializeObject implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7507585085783455597L;
+	int nummer;
+	String name;
+
+	public TestSerializeObject(int nummer, String name) {
+		this.nummer = nummer;
+		this.name = name;
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//testobjekt erzeugen
+		TestSerializeObject t1 = new TestSerializeObject(1, "Max");
+		
+		//Objekt speichern
+		try {
+			
+			//Evtl Pfad angeben
+			File file = new File("meineTestObjekte.to");
+			FileOutputStream fos = new FileOutputStream(file);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(t1);
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
