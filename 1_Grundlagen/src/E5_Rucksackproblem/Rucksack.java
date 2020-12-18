189f40f82f1f7de22ceac28e758448fcc7013c6c
nh <n.hammer@online.de>
2020-12-18 07:45:29 +0100

    Rucksackproblem added

---
+++ 1_Grundlagen/src/E5_Rucksackproblem/Rucksack.java
@@ -0,0 +1,14 @@
+package E5_Rucksackproblem;
+
+public class Rucksack {
+
+	public Rucksack() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
