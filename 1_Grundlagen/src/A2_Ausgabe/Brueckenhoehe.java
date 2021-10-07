6ec2adc54d5a20a1553445890fe0b4f4964fdfce
hammerniko <hammer.nikolai@googlemail.com>
2021-10-07 11:57:33 +0200

    added

---
+++ 1_Grundlagen/src/A2_Ausgabe/Brueckenhoehe.java
@@ -0,0 +1,14 @@
+package A2_Ausgabe;
+
+public class Brueckenhoehe {
+
+	public static void main(String[] args) {
+		System.out.println("Die Hoehe einer Bruecke");
+		System.out.println("ergibt sich aus s=0.5*g*t²");
+		System.out.println();
+		System.out.println("g = 9.81 m/s");
+		System.out.println("t = 2.5s  \t\t   (Fallzeit eines Steines von der Bruecke)");
+		System.out.println("s = "+(0.5*9.81*2.5*2.5)+"m    (Hoehe der Bruecke)");
+	}
+
+}
