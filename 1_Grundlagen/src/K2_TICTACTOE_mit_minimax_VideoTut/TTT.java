b9ce5223b68cdf0b57281771a08c68c417a5bb15
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2022-07-07 11:11:31 +0200

    modified

--- 1_Grundlagen/src/K2_TICTACTOE_mit_minimax_VideoTut/TTT.java
+++ 1_Grundlagen/src/K2_TICTACTOE_mit_minimax_VideoTut/TTT.java
@@ -8,33 +8,36 @@ package K2_TICTACTOE_mit_minimax_VideoTut;
  */
 public class TTT {
 
+	public static int minimumX=-1; //opt. x Position für nächsten Zug des PC 
+	public static int minimumY=-1; //opt. y Position für nächsten Zug des PC 
 	public static final int NICHT_FERTIG = -1;
-	public static final int S1 = 1; // verwendet min()
-	public static final int S2 = 2; // verwendet max()
+	public static final int PC = 1; // Computer verwendet min() 
+	public static final int S2 = 2; // Person verwendet max()
 	public static final int FREI = 0;
 	public static final int S1_GEWONNEN = 0;
-	public static final int S2_GEWONNEN = 2;
+	public static final int PC_GEWONNEN = 2;
 	public static final int UNENTSCHIEDEN = 1;
 	public static int[][] sf = { 
-			{ 0, 1, 0 }, 
-			{ 2, 2, 1 }, 
-			{ 2, 2, 1 } };
+			{ 2, 0, 0 }, 
+			{ 0, 0, 0 }, 
+			{ 0, 0, 0 }};
 
 	public static void main(String[] args) {
 		ausgabe();
-
+ 
 		System.out.println(min());
+		System.out.println(minimumX);
+		System.out.println(minimumY);
 	}
-
 	private static void ausgabe() {
 		char z = '-';
-		for (int i = 0; i < sf.length; i++) {
-			for (int j = 0; j < sf.length; j++) {
+		for (int y = 0; y < sf.length; y++) {
+			for (int x = 0; x < sf.length; x++) {
 
-				if (sf[i][j] == S1)
-					z = 'O';
-				else if (sf[i][j] == S2)
-					z = 'X';
+				if (sf[y][x] == PC)
+					z = 'O'; //Computer
+				else if (sf[y][x] == S2)
+					z = 'X'; //Spieler
 				else
 					z = '-';
 
@@ -58,22 +61,22 @@ public class TTT {
 
 		// Gewinner prüfen
 		for (int i = 0; i < sf.length; i++) {
-			if (sf[i][0] == S1 && sf[i][1] == S1 && sf[i][2] == S1)
+			if (sf[i][0] == PC && sf[i][1] == PC && sf[i][2] == PC)
 				return S1_GEWONNEN;
-			if (sf[0][i] == S1 && sf[1][i] == S1 && sf[2][i] == S1)
+			if (sf[0][i] == PC && sf[1][i] == PC && sf[2][i] == PC)
 				return S1_GEWONNEN;
-			if (sf[0][0] == S1 && sf[1][1] == S1 && sf[2][2] == S1)
+			if (sf[0][0] == PC && sf[1][1] == PC && sf[2][2] == PC)
 				return S1_GEWONNEN;
-			if (sf[0][2] == S1 && sf[1][1] == S1 && sf[2][0] == S1)
+			if (sf[0][2] == PC && sf[1][1] == PC && sf[2][0] == PC)
 				return S1_GEWONNEN;
 			if (sf[i][0] == S2 && sf[i][1] == S2 && sf[i][2] == S2)
-				return S2_GEWONNEN;
+				return PC_GEWONNEN;
 			if (sf[0][i] == S2 && sf[1][i] == S2 && sf[2][i] == S2)
-				return S2_GEWONNEN;
+				return PC_GEWONNEN;
 			if (sf[0][0] == S2 && sf[1][1] == S2 && sf[2][2] == S2)
-				return S2_GEWONNEN;
+				return PC_GEWONNEN;
 			if (sf[0][2] == S2 && sf[1][1] == S2 && sf[2][0] == S2)
-				return S2_GEWONNEN;
+				return PC_GEWONNEN;
 		}
 
 		// Auf leere Felder prüfen
@@ -89,22 +92,29 @@ public class TTT {
 		return UNENTSCHIEDEN;
 
 	}
-
+	
+	/**
+	 * Wählt die Position die für den zugeordneten Spieler das
+	 * Maximum von der Auswertung liefert.
+	 * @return 2 bester Zug für Spieler S2
+	 * @return 0 schlechtester Zug für Spieler S2
+	 * @return 1 unentschieden
+	 */
 	public static int max() {
 		int maximalwert = -999;
 		int wert;
 		if (auswertung() != NICHT_FERTIG)
 			return auswertung();
 
-		for (int i = 0; i < sf.length; i++) {
-			for (int j = 0; j < sf.length; j++) {
-				if (sf[i][j] == FREI) {
-					sf[i][j] = S2;
+		for (int y = 0; y < sf.length; y++) {
+			for (int x = 0; x < sf.length; x++) {
+				if (sf[y][x] == FREI) {
+					sf[y][x] = S2;
 					wert = min();
-					if (wert > maximalwert) {
+					if (wert >= maximalwert) {
 						maximalwert = wert;
 					}
-					sf[i][j] = FREI;
+					sf[y][x] = FREI;
 				}
 			}
 		}
@@ -112,27 +122,34 @@ public class TTT {
 		return maximalwert;
 	}
 
+	
+	 /**
+	 * Wählt die Position die für den PC das
+	 * Minimum von der Auswertung liefert.
+	 * @return 0 bester Zug für Spieler S1
+	 * @return 2 schlechtester Zug für Spieler S1
+	 * @return 1 unentschieden
+	 */
 	private static int min() {
-		int minimalwert = 999;
+		int minimalwert = 255;
 		int wert;
 		if (auswertung() != NICHT_FERTIG)
 			return auswertung();
 
-		for (int i = 0; i < sf.length; i++) {
-			for (int j = 0; j < sf.length; j++) {
-				if (sf[i][j] == FREI) {
-					sf[i][j] = S1;
+		for (int y = 0; y < sf.length; y++) {
+			for (int x = 0; x < sf.length; x++) {
+				if (sf[y][x] == FREI) {
+					sf[y][x] = PC;
 					wert = max();
-					if (wert < minimalwert) {
+					if (wert <= minimalwert) {
 						minimalwert = wert;
-						
+						minimumX = x;
+						minimumY = y;
 					}
-					sf[i][j] = FREI;
+					sf[y][x] = FREI;
 				}
-
 			}
 		}
-
 		return minimalwert;
 	}
 }
