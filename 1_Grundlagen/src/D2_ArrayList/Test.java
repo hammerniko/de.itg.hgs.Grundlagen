2d6569e415c360db0ae90592155a27497ae0b98a
hr <hr@a206-lehrer.ad.singen-hgs.logodidact.net>
2023-05-17 12:59:32 +0200

    added ArrayList

---
+++ 1_Grundlagen/src/D2_ArrayList/Test.java
@@ -0,0 +1,23 @@
+package D2_ArrayList;
+
+import java.util.ArrayList;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> klasseTg113 = new ArrayList<String>();
+		
+		klasseTg113.add("Max");
+		klasseTg113.add("Michael");
+		
+		
+		for (int i = 0; i < klasseTg113.size(); i++) {
+			System.out.println(klasseTg113.get(i));
+		}
+		
+		
+
+	}
+
+}
