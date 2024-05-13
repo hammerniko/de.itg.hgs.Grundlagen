80ef168fe12733d8674ab74d0d7cf16b093ac150
Nikolai Hammer <hammer.nikolai@googlemail.com>
2024-05-13 06:29:10 +0000

    added

---
+++ 1_Grundlagen/src/h4_lambda/KA1_lambda.java
@@ -0,0 +1,17 @@
+package h4_lambda;
+
+/**
+ * KA1_lambda
+ */
+public class KA1_lambda {
+
+    interface Rectangle{
+        public double getArea(double length, double width);
+    }
+
+    public static void main(String[] args) {
+       
+        System.out.println("Rechteck berechnen");
+      
+    }
+}
